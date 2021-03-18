0536f747f11a8b373dfb0ae006598af2bf025a86
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-18 10:33:26 +0100

    Added ex10 src and index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var myMusic = {
+      001: {
+  artist: "Billy Joel",
+  title: "Piano Man",
+  release_year: 1973,
+  formats: {
+      1: "CD",
+      2: "8T",
+      3: "LP"
+  },
+  gold: true
+},
+002: { 
+    
+}
+
+
+};
+return myMusic;
+}
+myFunction()[2];
+console.log(myFunction()[2]);
+module.exports = myFunction;
